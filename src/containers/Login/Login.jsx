494dab0ee12e224c37edd244a210d59df00b45c6
Developer <sharamradov@gmail.com>
2022-07-07 12:08:00 +0300

    add redux store for token

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -3,14 +3,23 @@ import { Form, Input } from 'antd';
 import clases from "./Login.module.scss";
 import { userLogin } from '../../services/userAPI';
 import ButtonBlack from '../../components/ButtonBlack/ButtonBlack';
+import { useDispatch } from 'react-redux/es/exports';
+import { setUser } from '../../store/slices/userSlice/userSlice';
 
 const Login = () => {
     const [login, setLogin] = useState('')
     const [password, setPassword] = useState('')
-    
-    const signIn = async () =>{
-        const user = await userLogin(login, password)
-        console.log(user)
+    const dispatch = useDispatch();
+    const signIn = (login, password) => {
+        userLogin(login, password)
+        .then(({data}) => {
+            console.log(data);
+            dispatch(setUser({
+                token: data.token,
+            }));
+            // push('/');
+        })
+        .catch(() => alert('Invalid user!'))  
     }
     return (
         <div className={clases.login}>
--- src/services/userAPI.js
+++ src/services/userAPI.js
@@ -1,11 +1,12 @@
 import {$host} from "../api/index";
-import jwt_decode from "jwt-decode";
+// import jwt_decode from "jwt-decode";
 
 export const userLogin = async (login, password) => {
     const {data} = await $host.post('api/user/signin', {login, password})
     console.log(data)
-    localStorage.setItem('token', data.token)
-    return jwt_decode(data.token)
+    // localStorage.setItem('token', data.token)
+    // return jwt_decode(data.token)
+    return data
 }
 
 
